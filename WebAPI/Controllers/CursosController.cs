b185b6fbb65b5a45e6e8e44990de30d147b96652
Henry García <henrygarciaospina@gmail.com>
2024-02-22 19:23:39 -0500

    Se corrige el  nombre del controller en plural CursosControler

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -16,19 +16,21 @@ public class CursosController : ControllerBase
     _mediator = mediator;
   }
 
-  /* http://localhost:5200/api/Curso */ 
+  /* http://localhost:5200/api/Cursos */ 
   [HttpGet]
   public async Task<ActionResult<List<Curso>>> Get() 
   {
     return await _mediator.Send(new Consulta.ListaCursos());
   }
 
-  /* http://localhost:5200/api/Curso/1 */
+  /* http://localhost:5200/api/Cursos/1 */
   [HttpGet("{id}")]
   public async Task<ActionResult<Curso>> GetById(int id)
   {
     return await  _mediator.Send(new ConsultaPorId.CursoUnico{Id = id});
   }
+
+  /* http://localhost:5200/api/Cursos */
   [HttpPost]
   public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
   {

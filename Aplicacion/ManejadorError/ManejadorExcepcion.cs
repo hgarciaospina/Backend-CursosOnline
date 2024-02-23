7f459e34d34b155dbf79df6ee5107a81c5d96c32
Henry García <henrygarciaospina@gmail.com>
2024-02-23 13:10:33 -0500

    Definición de un Middleware para el manejo de errores de la API

---
+++ Aplicacion/ManejadorError/ManejadorExcepcion.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Aplicacion.ManejadorError
+{
+    public class ManejadorExcepcion : Exception
+    {
+        public HttpStatusCode Codigo {get;}
+        public object Errores {get;}
+        public ManejadorExcepcion(HttpStatusCode codigo, object errores)
+        {
+            Codigo = codigo;
+            Errores = errores;
+        }
+    }
+}

7806cae700169fcd7462a76498052a1a274c1350
Henry García <henrygarciaospina@gmail.com>
2024-02-26 18:38:55 -0500

    Definición del formato para los atributos Promocion y PrecioActual

--- Dominio/Precio.cs
+++ Dominio/Precio.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace Dominio
     public class Precio
     {
         public Guid PrecioId {get; set;}
+
+        [Column(TypeName = "decimal(12,2)")]
         public decimal PrecioActual {get; set;}
+
+        [Column(TypeName = "decimal(12,2)")]
         public decimal Promocion {get; set;}
         public Guid CursoId {get; set;}
         public required Curso Curso {get; set;}

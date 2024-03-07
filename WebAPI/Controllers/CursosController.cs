3aaa72a751fba6e0ab3d4ad5f632c88cc29d585c
Henry García <henrygarciaospina@gmail.com>
2024-03-07 00:40:59 -0500

    Configuration GenericBaseController

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -7,34 +7,29 @@ namespace WebAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CursosController : ControllerBase
+public class CursosController : GenericBaseController
 {
   
-  private readonly IMediator _mediator;
-  public CursosController(IMediator mediator)
-  {
-    _mediator = mediator;
-  }
 
   /* http://localhost:5200/api/Cursos */ 
   [HttpGet]
   public async Task<ActionResult<List<Curso>>> Get() 
   {
-    return await _mediator.Send(new Consulta.ListaCursos());
+    return await Mediator.Send(new Consulta.ListaCursos());
   }
 
   /* http://localhost:5200/api/Cursos/1 */
   [HttpGet("{id}")]
   public async Task<ActionResult<Curso>> GetById(int id)
   {
-    return await  _mediator.Send(new ConsultaPorId.CursoUnico{Id = id});
+    return await  Mediator.Send(new ConsultaPorId.CursoUnico{Id = id});
   }
 
   /* http://localhost:5200/api/Cursos */
   [HttpPost]
   public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
   {
-    return await _mediator.Send(data);
+    return await Mediator.Send(data);
   }
 
   /* http://localhost:5200/api/Cursos/1 */
@@ -42,13 +37,13 @@ public class CursosController : ControllerBase
   public async Task<ActionResult<Unit>> Editar(int id, Editar.Ejecuta data)
   {
     data.CursoId = id;
-    return await  _mediator.Send(data);
+    return await  Mediator.Send(data);
   }
 
   /* http://localhost:5200/api/Cursos/1 */
   [HttpDelete("{id}")]
   public async Task<ActionResult<Unit>> Eliminar(int id)
   {
-    return await _mediator.Send(new Eliminar.Ejecuta{Id = id});
+    return await Mediator.Send(new Eliminar.Ejecuta{Id = id});
   }
 }
